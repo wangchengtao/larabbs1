18d6aa2b7e8c3937bcf6c6fc807f57f3f19f9752
wangchengtao <915129420@qq.com>
2019-04-02 18:03:01 +0800

    用户话题列表

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Topic;
+use Illuminate\Http\Request;
+
+class CategoriesController extends Controller
+{
+    public function show(Category $category, Request $request)
+    {
+        $topics = Topic::where('category_id', $category->id)->withOrder($request->order)->paginate(20);
+
+        return view('topics.index', compact('topics', 'category'));
+    }
+}
