ae21dd8bc0dc117023fd05cd19de1e293a280e25
wangchengtao <915129420@qq.com>
2019-04-02 17:00:56 +0800

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,14 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'category_id', 'order', 'excerpt', 'slug'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use \App\Models\User;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = app(Faker\Generator::class);
+
+        // 头像假数据
+        $avatars = [
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/Lhd1SHqu86.png',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/LOnMrqbHJn.png',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/xAuDMxteQy.png',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/NDnzMutoxX.png',
+        ];
+
+        $users = factory(User::class)
+            ->times(10)
+            ->make()
+            ->each(function ($user) use ($faker, $avatars)
+            {
+                $user->avatar = $faker->randomElement($avatars);
+            });
+
+        // 让隐藏字段可见
+        $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
+
+        DB::table('users')->insert($user_array);
+
+        // 单独处理第一个用户的数据
+        $user = User::find(1);
+        $user->name = 'wangct';
+        $user->email = '915129420@qq.com';
+        $user->avatar = 'https://iocaffcdn.phphub.org/uploads/avatars/20121_1511515154.jpeg!/both/380x380';
+        $user->save();
+    }
+}
