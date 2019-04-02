39c77fe0846e3dcefa143db5368e1719a40cbd0c
wangchengtao <915129420@qq.com>
2019-04-02 16:45:45 +0800

    填充用户和话题数据

--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -7,10 +7,17 @@ class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
+        $faker = app(Faker\Generator::class);
+
+        $user_ids = \App\Models\User::all()->pluck('id')->toArray();
+        $category_ids = \App\Models\Category::all()->pluck('id')->toArray();
+
+        $topics = factory(Topic::class)
+            ->times(100)
+            ->make()
+            ->each(function ($topic) use ($faker, $user_ids, $category_ids) {
+                $topic->user_id = $faker->randomElement($user_ids);
+                $topic->category_id = $faker->randomElement($category_ids);
         });
 
         Topic::insert($topics->toArray());
