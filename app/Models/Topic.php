8e44ab18b38c4f7207e94ae778fa948daba236cf
wangchengtao <915129420@qq.com>
2019-04-02 17:55:56 +0800

    话题排序

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
+
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'category_id', 'order', 'excerpt', 'slug'];
@@ -14,5 +16,30 @@ class Topic extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function scopeWithOrder(Builder $query, $order)
+    {
+        // 不同的排序, 使用不同的数据读取逻辑
+        switch ($order) {
+            case 'recent':
+                $query->recent();
+                break;
+            default:
+                $query->recentReplied();
+                break;
+        }
+
+        return $query->with('user', 'category');
+    }
+
+    public function scopeRecent($builder)
+    {
+        return $builder->orderBy('created_at', 'desc');
+    }
+
+    public function scopeRecentReplied(Builder $builder)
+    {
+        return $builder->orderBy('updated_at', 'desc');
     }
 }
