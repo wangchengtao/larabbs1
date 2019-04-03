07aba8a5ae11fe3294ed592ae843c144ae6af42e
wangchengtao <915129420@qq.com>
2019-04-03 15:33:10 +0800

    删除评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -19,18 +19,22 @@ class ReplyObserver
 
     public function created(Reply $reply)
     {
-        $reply->topic->reply_count = $reply->topic->replies->count();
-        $reply->topic->save();
+        $reply->topic->updateReplyCount();
 
         // 通知话题作者有新的评论
-        // $reply->topic->user->notify(new TopicReplied($reply));
+        $reply->topic->user->notify(new TopicReplied($reply));
 
-        $user_ids = $reply->topic->replies->pluck('user_id');
-        Notification::send(User::find($user_ids), new TopicReplied($reply));
+        // $user_ids = $reply->topic->replies->pluck('user_id');
+        // Notification::send(User::find($user_ids), new TopicReplied($reply));
     }
 
     public function updating(Reply $reply)
     {
         //
+    }
+
+    public function deleted(Reply $reply)
+    {
+        $reply->topic->updateReplyCount();
     }
 }
