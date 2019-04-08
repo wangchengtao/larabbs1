1bf643b4e571b431a533f812aa82012a64d973e6
wangchengtao <915129420@qq.com>
2019-04-08 16:43:11 +0800

    首页话题列表

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
 
 // 用户身份验证相关的路由
