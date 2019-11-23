88b3b67ae49f34790a1a66ae8b6b5590acb9ad5f
wangchengtao <915129420@qq.com>
2019-11-23 23:46:29 +0800

    第三方登录

---
+++ _ide_helper_models.php
@@ -0,0 +1,184 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * App\Models\Category
+ *
+ * @property int $id
+ * @property string $name 名称
+ * @property string|null $description 描述
+ * @property int $post_count 帖子数
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Category newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Category newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Category query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Category whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Category whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Category whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Category wherePostCount($value)
+ */
+	class Category extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Link
+ *
+ * @property int $id
+ * @property string $title 资源的描述
+ * @property string $link 资源的链接
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Link newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Link newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Link query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Link whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Link whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Link whereLink($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Link whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Link whereUpdatedAt($value)
+ */
+	class Link extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Model
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Model newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Model newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Model ordered()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Model query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Model recent()
+ */
+	class Model extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Reply
+ *
+ * @property int $id
+ * @property int $topic_id
+ * @property int $user_id
+ * @property string $content
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Topic $topic
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Reply newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Reply newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Model ordered()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Reply query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Model recent()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Reply whereContent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Reply whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Reply whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Reply whereTopicId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Reply whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Reply whereUserId($value)
+ */
+	class Reply extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Topic
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $body
+ * @property int $user_id
+ * @property int $category_id
+ * @property int $reply_count
+ * @property int $view_count
+ * @property int $last_reply_user_id
+ * @property int $order
+ * @property string|null $excerpt
+ * @property string|null $slug
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Category $category
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Reply[] $replies
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Model ordered()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic recent()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic recentReplied()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereExcerpt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereLastReplyUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereOrder($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereReplyCount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereViewCount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic withOrder($order)
+ */
+	class Topic extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $phone
+ * @property string|null $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string|null $password
+ * @property string|null $weixin_openid
+ * @property string|null $weixin_unionid
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $avatar
+ * @property string|null $introduction
+ * @property int $notification_count
+ * @property string|null $last_actived_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Spatie\Permission\Models\Permission[] $permissions
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Reply[] $replies
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Spatie\Permission\Models\Role[] $roles
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Topic[] $topics
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User permission($permissions)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User role($roles, $guard = null)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereAvatar($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereIntroduction($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereLastActivedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereNotificationCount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereWeixinOpenid($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereWeixinUnionid($value)
+ */
+	class User extends \Eloquent {}
+}
+
