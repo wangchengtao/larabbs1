72af7be65a4e3254dc3deae86bd4f2cd50340c59
wangchengtao <915129420@qq.com>
2019-12-02 23:54:41 +0800

    话题列表

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\Api\TopicRequest;
 use App\Models\Topic;
+use App\Models\User;
 use App\Transformers\TopicTransformer;
+use Illuminate\Http\Request;
 
 class TopicsController extends Controller
 {
@@ -32,6 +34,35 @@ class TopicsController extends Controller
 
         $topic->delete();
         return $this->response->noContent();
+    }
 
+    public function index(Request $request, Topic $topic)
+    {
+        $query = $topic->query();
+
+        if ($categoryId = $request->categor_id) {
+            $query->where('category_id', $categoryId);
+        }
+
+        switch ($request->order) {
+            case 'recent':
+                $query->recent();
+                break;
+            default:
+                $query->recentReplied();
+                break;
+        }
+
+        $topics = $query->paginate(20);
+
+        return $this->response->paginator($topics, new TopicTransformer());
+    }
+
+    public function userIndex(Request $request, User $user)
+    {
+        $topics = $user->topics()->recent()
+                       ->paginate(20);
+
+        return $this->response->paginator($topics, new TopicTransformer());
     }
 }
